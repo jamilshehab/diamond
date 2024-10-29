54da86027dde08b50674168dad20158a4223fd2a
jamilshehab <jamilshehab>
2024-10-29 22:09:47 +0200

    changes added

---
+++ diamond.py
@@ -0,0 +1,17 @@
+num=int(input("enter the number of rows : "))
+for i in range(0,num+1):
+   for j in range(0,num-i):
+       print(" ",end="  ")
+
+   for j in range(1,2*i):
+       print("*" , end="  ")
+   print()
+
+for i in range(num,0,-1):
+   for j in range(num-i):
+       print(" ",end="  ")
+
+   for j in range(2*i-1):
+       print("*" , end="  ")
+   print()
+
---
+++ readme.md
@@ -0,0 +1,9 @@
+Write a Python program that prompts the user to enter a number of rows and then generates a symmetrical "palindrome" star pattern, centered to form a diamond shape. Example: 
+For rows = 4, 
+   *
+  ***
+ *****
+*******
+ *****
+  ***
+   *
